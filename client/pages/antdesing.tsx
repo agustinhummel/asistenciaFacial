ca203b5a34a4bbcf931381c3a2509b3151726857
agustin hummel <104798688+agustinhummel@users.noreply.github.com>
2024-04-20 12:34:01 -0300

    home/doctor

--- client/pages/antdesing.tsx
+++ client/pages/antdesing.tsx
@@ -6,10 +6,12 @@ import { Button } from 'antd';
 const Antdesing: React.FC = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
-    <h1 className="text-3xl font-bold mb-4">Test page for antdesign</h1>
-    <p className="mb-8">ssssss.</p>
-    <Button type="primary" className="mt-4">este boton deberia mostrarse pero esta en conflicto con tailwind y antd</Button>
-  </div>
+      <h1 className="text-3xl font-bold mb-4">Test page for antdesign</h1>
+      <p className="mb-8">ssssss.</p>
+      <Button className="mt-4 bg-red-800">
+        este boton deberia mostrarse pero esta en conflicto con tailwind y antd
+      </Button>
+    </div>
   );
 };
 
